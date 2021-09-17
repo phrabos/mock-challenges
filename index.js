6f67cd065906b415eaa921a139cc5ab9c6d8405a
patrick hrabos <phrabos@gmail.com>
2021-09-16 21:16:44 -0700

    delete answers

--- index.js
+++ index.js
@@ -1,102 +1,20 @@
-const sum = (...args) => {
-	return args.reduce((acc, curr) => {
-		acc += curr;
-		return acc;
-	}, 0);
-};
+const sum = (...args) => {};
 
 const vowelize = (arr) => {
 	let vowelRegex = /[aeiou]/gi;
-
-	return arr
-		.map((word) => {
-			if (!word.match(vowelRegex)) return '';
-			return word.replace(/[^aeiou]/gi, '');
-		})
-		.filter((el) => el !== '');
 };
-const combineAndSort = (...args) => {
-	return args.flat().sort();
-};
-const anagramTester = (a, b) => {
-	// console.log('string');
-	const firstWordLetterCount = a
-		.toLowerCase()
-		.split('')
-		.sort()
-		.reduce((acc, curr) => {
-			if (!acc[curr]) acc[curr] = 1;
-			else acc[curr]++;
-			return acc;
-		}, {});
-	const secondWordLetterCount = b
-		.toLowerCase()
-		.split('')
-		.sort()
-		.reduce((acc, curr) => {
-			if (!acc[curr]) acc[curr] = 1;
-			else acc[curr]++;
-			return acc;
-		}, {});
-	const stringA = JSON.stringify(firstWordLetterCount);
+const combineAndSort = (...args) => {};
+const anagramTester = (a, b) => {};
 
-	const stringB = JSON.stringify(secondWordLetterCount);
+const objectForEach = (obj, callback) => {};
 
-	return stringA === stringB;
-};
+const updateAtPath = (obj, string, value) => {};
 
-const objectForEach = (obj, callback) => {
-	const keys = Object.keys(obj);
-	keys.forEach((key) => callback(obj[key]));
-};
+class Car {}
 
-const updateAtPath = (obj, string, value) => {
-	let path = string.split('.');
-	for (let i = 0; i < path.length; i++) {
-		if (i !== path.length - 1) {
-			obj = obj[path[i]];
-		} else obj[path[i]] = value;
-	}
-};
+const whatWouldYouLikeToCheckOut = async (libraryApi) => {};
 
-class Car {
-	initialFuelLevel;
-	fuelCapacity;
-	constructor({ initialFuelLevel, fuelCapacity }) {
-		(this.initialFuelLevel = initialFuelLevel),
-			(this.fuelCapacity = fuelCapacity);
-	}
-	getFuelLevel() {
-		return this.initialFuelLevel;
-	}
-	addFuel(num) {
-		num + this.initialFuelLevel <= this.fuelCapacity
-			? (this.initialFuelLevel += num)
-			: (this.initialFuelLevel = this.fuelCapacity);
-	}
-}
-
-const whatWouldYouLikeToCheckOut = async (libraryApi) => {
-	const authors = await libraryApi.favoriteAuthors();
-	const books = await Promise.all(
-		authors.map((author) => libraryApi.booksAvailableBy(author))
-	);
-	const titles = [];
-	for (t of books.flat()) {
-		titles.push(t.title);
-	}
-
-	return titles;
-};
-
-const calc = (left) => {
-	return function mid(middle) {
-		return function r(right) {
-			console.log(left, middle, right);
-			return eval(`${left}${middle}${right}`);
-		};
-	};
-};
+const calc = (left) => {};
 
 module.exports = {
 	exampleFunction: function (message) {
